08c575fc8d7a0bb4363380dc67a5311a57296754
MahdiFard30 <lm222006@gmail.com>
2025-09-14 17:23:35 +0200

    Remove build

--- av24tr-team-c-main/build/CMakeFiles/use_uart.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/use_uart"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/use_uart.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
