1d80bbb5e18511d98288e7f70948ee22bbcf7abb
MahdiFard30 <lm222006@gmail.com>
2025-09-14 17:37:15 +0200

    Save local changes before rebase

---
+++ av24tr-team-c-main/build/CMakeFiles/use_tcp.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/use_tcp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/use_tcp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ av24tr-team-c-main/build/CMakeFiles/use_uart.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/use_uart"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/use_uart.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
