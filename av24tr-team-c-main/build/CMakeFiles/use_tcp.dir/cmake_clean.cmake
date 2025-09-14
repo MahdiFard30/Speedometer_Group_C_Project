aefd57f2174b0ded1550f2d0611d0c8f5718ba54
MahdiFard30 <lm222006@gmail.com>
2025-09-14 17:39:03 +0200

    Remove build directory and ignore it

--- av24tr-team-c-main/build/CMakeFiles/use_tcp.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/use_tcp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/use_tcp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
